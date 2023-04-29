f166ade5b8b58368246fb7840ee89ce7206cc670
Mr.Makako <diegofdiaz1717@gmail.com>
2023-04-29 16:57:44 -0600

    rutas principales listas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 function App() {
-  ///
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route
+          path="Home"
+          index
+          element={
+            <>
+              <h1>Inicio</h1>
+            </>
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <>
+              <h1>Login</h1>
+            </>
+          }
+        />
+        <Route
+          path="signUp"
+          element={
+            <>
+              <h1>SignUp</h1>
+            </>
+          }
+        />
+        <Route
+          path="dashboard"
+          element={
+            <>
+              <h1>Dashboard</h1>
+            </>
+          }
+        />
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
