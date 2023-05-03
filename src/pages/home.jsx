395c1a42b21539ac1601a10d88475cf7a1c6a9c2
Mr.Makako <diegofdiaz1717@gmail.com>
2023-05-03 17:53:47 -0600

    ready up

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,6 @@ function Home() {
       <div className="register">
         <div className="col-1">
           <div id="form" className="flex flex-col">
-            <button className="btn">Register</button>
             <Link to="/register">
               <button className="btn">Register</button>
             </Link>
