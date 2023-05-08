5ddfeb5ceea1489127224a7917ee8298ba983f14
dessire <dessireochoa12@gmail.com>
2023-05-08 13:47:15 -0600

    Pequeño cambio

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -55,28 +55,29 @@ function Dashboard() {
     }
 
     const itemID = evt.dataTransfer.getData("itemID");
-    const item = tasks.find((item) => item.id == itemID);
+    const item = tasks.find((item) => item.id === Number(itemID));
     const oldList = item.list;
 
     // get the index of the item being dragged
-    const oldIndex = tasks.findIndex((item) => item.id == itemID);
+    const oldIndex = tasks.findIndex((item) => item.id === Number(itemID));
+
+    // create a copy of tasks and remove the item from the old position
+    const tasksCopy = [...tasks];
+    tasksCopy.splice(oldIndex, 1);
 
     // get the new index of the item in the list
     const newIndex = Array.from(evt.target.parentNode.children).indexOf(
       evt.target
     );
 
-    // remove the item from the old position
-    tasks.splice(oldIndex, 1);
-
     // insert the item into the new position
-    tasks.splice(newIndex, 0, item);
-
-    // update the list property of the item
-    item.list = list;
+    tasksCopy.splice(newIndex, 0, {
+      ...item,
+      list,
+    });
 
     // update the state with the new task list
-    setTasks([...tasks]);
+    setTasks(tasksCopy);
   };
 
   return (
