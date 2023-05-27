d21551d16a493d344ee632db348e5f7ef333b1bf
Mr.Makako <diegofdiaz1717@gmail.com>
2023-05-26 21:32:00 -0600

    changes on  structure new request folder for rquest handlers.

--- src/pages/components/stack.js
+++ src/pages/components/stack.js
@@ -1,17 +1,42 @@
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+function createCard(board_name) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {board_name}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="big">Go</Button>
+      </CardActions>
+    </Card>
+  );
+}
 function Pile(user_boards) {
   return (
     <Box sx={{ width: "100%" }}>
       <Stack spacing={2}>
         {user_boards.map((data) => (
-          <Button>
-            {data.id}-{data.name} creado por:{data.user_id}
-          </Button>
+          <>{createCard(data.name)}</>
         ))}
       </Stack>
     </Box>
